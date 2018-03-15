d6b224588e0f6c64d279d1554879da31e5b1661c
SyauqyLenterano <syauqilenterano@gmail.com>
2018-03-15 22:20:25 +0700

    deleting some comment

--- c++/main.cc
+++ c++/main.cc
@@ -17,9 +17,7 @@ int main(){
 	
 	double h=20.0;
 	double *Vel_Mod = new double [nrows*ncols];
-	double *f_src = new double [nrows*ncols];
-	double **P=alloc_mat(nt,nrows*ncols);
-	
+
 	//Create Uniform Velocity model
 	for (int i=0; i<nrows;i++){
 		for (int j=0;j<ncols;j++){
@@ -31,11 +29,6 @@ int main(){
 	double fmax=50.0;
 	double t;
 	int src_loc=(ncols)*nrows/2+ncols/2;
-	std::cout<< " Time domain iteration is done\n";
-	//w_dat("data/convFD",Vel_Mod,P,dt,h,nt,nrows,1,ncols);
 
-	//free memories
 	delete [] Vel_Mod;
-	delete [] f_src;
-	free_mat_mem(P);
 }
