72e21a79d3abd75447ab520c64c88274b4bce111
SyauqyLenterano <syauqilenterano@gmail.com>
2018-03-16 00:24:02 +0700

    deleting nadm.* file

--- c++/nadm.h
+++
@@ -1,7 +0,0 @@
-#ifndef NADM_H
-#define NADM_H
-double *cdth2(double *Vel_Mod,double dt,double h,int nx,int ny, int nz);
-double *cdth4(double *Vel_Mod,double dt,double h,int nx,int ny, int nz);
-//double **nadm_prog_2d(int src_loc,double freq,double *Vel_Mod,double dt, int nstep, double h, int nx, int nz);
-//double **nadm_prog_3d(int src_loc,double freq,double *Vel_Mod,double dt, int nstep, double h, int nx, int ny, int nz);
-#endif
