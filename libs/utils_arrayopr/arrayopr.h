b8acd4ffaee90e783061427218a7b6801e55b625
SyauqyLenterano <syauqilenterano@gmail.com>
2018-05-27 00:37:42 +0700

    delete unnecessery files

--- libs/utils_arrayopr/arrayopr.h
+++
@@ -1,7 +0,0 @@
-#ifndef ARRAYOPR_H
-#define ARRAYOPR_H
-float *vek_addition(float *A, float *B,const int nelmts);
-float *vek_subtraction(float *A, float *B,const int nelmts);
-float *vek_elmwise_mult(float *A, float *B,const int nelmts);
-float *scalar_mult(float scalar_const,float *A,const int nelmts);
-#endif
