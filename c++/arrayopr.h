1e1de6ba10040cd915e987a41bf608df44030428
SyauqyLenterano <syauqilenterano@gmail.com>
2018-05-27 00:16:19 +0700

    deleting old files

--- c++/arrayopr.h
+++
@@ -1,7 +0,0 @@
-#ifndef ARRAYOPR_H
-#define ARRAYOPR_H
-double *vek_addition(double *A, double *B,const int nelmts);
-double *vek_subtraction(double *A, double *B,const int nelmts);
-double *vek_elmwise_mult(double *A, double *B,const int nelmts);
-double *scalar_mult(double scalar_const,double *A,const int nelmts);
-#endif
