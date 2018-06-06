ca21dde56deed03af1971d9a66f29d99f887e78d
syauqi <syauqilenterano@gmail.com>
2018-06-07 06:27:46 +0700

    ggenerate profiler from aacer

--- 2d/main.cc
+++ 2d/main.cc
@@ -5,7 +5,7 @@
 #include "arrayman.h"
 #include "wavesim.h"
 
-int main(){
+int main(){/*
 	std::vector<double> vel;
 	std::vector<int> srcloc;
 	int nx,ny,nz,ns;
@@ -15,12 +15,16 @@ int main(){
 	int nt=int(T/dt+1);
 	std::cout<<nx<<" "<<ny<<" "<<nz<<" "<<nt<<" "<<ns<<" "<<dt<<" "<<T<<" "<<h<<" "<<fm<<"\n";
 	double *Velocity=&vel[0];
+	*/
+	double *Velocity=new double[50*50];
+	for(int i=0;i<2500;i++){Velocity[i]=4000;}
 	double **U;
 	for (int i=0;i<1;i++){
-		U=wvenacd(Velocity,nx,ny,srcloc[i],fm,h,dt,T);
-		write_rec("~/fin_proj_data/record/rec",U,srcloc[i],h,dt,nx,nt);
+		U=wvenacd(Velocity,50,50,25,50,10,0.001,0.5);
+		write_rec("~/fin_proj_data/record/rec",U,25,10,0.001,50,501);
 		}
 	//w_dat("/scratch/syauqy/data/sample",Velocity,U,dt,h,nt,nx,ny,1,1,-1,1);
 	free_mat_mem(U);
+	delete [] Velocity;
 	}
 
