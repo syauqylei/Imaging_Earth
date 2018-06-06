39c343c8714a5eac5add4f37504dcd66759a4867
Syauqy_Guriang <syauqi@guriang.unpad.ac.id>
2018-06-06 14:53:06 +0700

    try to compile with pgc++

--- 2d/source_function.cc
+++ 2d/source_function.cc
@@ -7,7 +7,6 @@ double ricker_wavelet_1(double freq, double t){
 }
 double ricker_wavelet_2(double freq, double t){
 	double f;
-	double pi=22.0/7.0;
     f = -5.76*freq*freq*(1-16.0*(0.6*freq*t-1)*(0.6*freq*t-1)) *exp(-8.0* (0.6*freq*t-1)*(0.6*freq*t-1));
     return f;
 }
