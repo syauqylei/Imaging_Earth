1a65de6aadf4d7782fd50ce3a52f153d56bfa549
syauqi <syauqilenterano@gmail.com>
2018-06-06 14:05:08 +0700

    try on BP2004 data set

--- 2d/main.cc
+++ 2d/main.cc
@@ -6,22 +6,18 @@
 #include "wavesim.h"
 
 int main(){
-	int nx=50;
-	int ny=50;
-	double h=10.0;
-	double dt=0.001;
-	double T=0.5;
-	double fm=50;
-	double *velocities;
+	std::vector<double> vel;
+	std::vector<int> srcloc;
+	int nx,ny,nz,ns;
+	double h,dt,T,fm;
+	read_vel("Velocity.txt",vel , nx,ny,nz,h);
+	read_fwdset("fwdset.txt",srcloc,ns,dt,T,fm);
+	int nt=int(T/dt+1);
+	std::cout<<nx<<" "<<ny<<" "<<nz<<" "<<nt<<" "<<ns<<" "<<dt<<" "<<T<<" "<<h<<" "<<fm<<"\n";
+	double *Velocity=&vel[0];
 	double **U;
-	double *vel=new double[nx*ny];
-	for (int i=0; i<nx*ny;i++){
-		vel[i]=4000.0;
-		}
-	U=wvenacd(vel,nx,ny,nx/2,50.0,10.0,0.001,0.5); 
-	int nt=int(0.5/0.001+1);
-	w_dat("data/sample",vel,U,0.001,10,nt,nx,ny,1);
+	U=wvenacd(Velocity,nx,ny,nx/2,fm,h,dt,T);
+	w_dat("data/sample",Velocity,U,dt,h,nt,nx,ny,1);
 	free_mat_mem(U);
-	delete [] vel;
 	}
 
--- 2d/srcloc_gen.sh
+++ 2d/srcloc_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for number in {676..1352..20}
+for number in {10..590..20}
 do
 echo "$number" >> fwdset.txt
 done
