de00bc974f9ae833b9641f5d05166094540906b8
Syauqy_Guriang <syauqi@guriang.unpad.ac.id>
2018-06-07 05:49:22 +0700

    edit rnw for write_rec func

--- 2d/rnw.cc
+++ 2d/rnw.cc
@@ -125,16 +125,16 @@ void write_vtk(const std::string& filename,double **U,double h,int nt,int nx, in
 	}
 }
 
-void write_record(std::string& filename, double **U,int srcloc, double h,double dt, int nx,int nt){
+void write_rec(const std::string& filename, double **U,int srcloc, double h,double dt, int nx,int nt){
 	std::ofstream file;
 	std::stringstream ss;
 	ss << std::setfill('0') << std::setw(5) << srcloc;
 	std::string ext1=".rec";
-	std::String ext2=".txt";
+	std::string ext2=".txt";
 	file.open(filename+ss.str()+ext1);
 	for (int i=0;i<nt;i++){
 		for (int j=0;j<nx;j++){
-		file << U[i][j]<<"\t";
+		file<<std::fixed()<<std::setprecision(5) << U[i][j]<<"\t";
 		}
 		std::cout<<"\n";
 	}
@@ -142,5 +142,5 @@ void write_record(std::string& filename, double **U,int srcloc, double h,double 
 	
 	file.open(filename+ss.str()+ext2);
 	file<<srcloc<<"\t"<<h<<"\t"<<nx<<"\t"<<"\t"<<nt<<"\n";
-	file.close
+	file.close();
 }
